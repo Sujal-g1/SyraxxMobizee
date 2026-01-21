7111fa250b82502639318d3822a157b695626a29
Sujal-g1 <sujal.work2005@gmail.com>
2026-01-21 23:24:20 +0530

    some change in firebaseadmin

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,14 @@
 // backend/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount =JSON.parse(process.env.serviceAccountKey);
+let serviceAccount;
+
+if (process.env.serviceAccountKey) {
+  // Production (Render) - read from env
+  serviceAccount = JSON.parse(process.env.serviceAccountKey);
+} else {
+  // Local development - read from file
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
